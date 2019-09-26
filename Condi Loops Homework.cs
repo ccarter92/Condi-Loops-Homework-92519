df9e0760e58df7e5d93ee45bae4793fc906292f3
ccarter92 <54338971+ccarter92@users.noreply.github.com>
2019-09-26 13:51:18 -0500

    Add files via upload

---
+++ Condi Loops Homework.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace CondiLoops
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //String Arrays
+            string[] brandnames =
+            {
+                "Fendi," "Louis Viutton," "Gucci,""Prada,""Moschino,"
+            };
+            Console.WriteLine(brandnames);
+
+            //Float Array
+
+            float[] doublenumbers =
+            {
+                13,25,13,32,62
+            };
+            
+            foreach(string item in brandnames)
+           if (float == 13)
+
+
+            }
+        }
+    }
+}
